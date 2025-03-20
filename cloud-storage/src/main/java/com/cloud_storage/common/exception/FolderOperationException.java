96eb2a3532d311ad26621305f5af5b6bfce94364
Ilya Lapin <lap1nilia@yandex.ru>
2025-03-20 15:41:10 +0300

    upload folder

---
+++ cloud-storage/src/main/java/com/cloud_storage/common/exception/FolderOperationException.java
@@ -0,0 +1,7 @@
+package com.cloud_storage.common.exception;
+
+public class FolderOperationException extends RuntimeException {
+    public FolderOperationException(String message) {
+        super(message);
+    }
+}
